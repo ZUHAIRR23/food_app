c36ab7d2c2167d63bce564cc5f4bb5619bbb7f79
Abdurahman Zuhair <abdurrahman.zuh@gmail.com>
2024-09-09 15:02:54 +0700

    Initial commit

---
+++ lib/ui/sign_up_page.dart
@@ -0,0 +1,137 @@
+part of 'pages.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwprdController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GeneralPage(
+        title: "Sign Up",
+        subtitle: "Create Your Account",
+        onBackButtonPressed: () {
+          Get.back();
+        },
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.fromLTRB(defaultMargin, 10, defaultMargin, 6),
+              child: Text(
+                "Name",
+                style: blackFontStyle2,
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: Colors.black,
+                ),
+              ),
+              child: TextField(
+                controller: nameController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintStyle: greyFontStyle,
+                    hintText: 'Type Your Name'),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.fromLTRB(defaultMargin, 10, defaultMargin, 6),
+              child: Text(
+                "Email Address",
+                style: blackFontStyle2,
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: Colors.black,
+                ),
+              ),
+              child: TextField(
+                controller: emailController,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintStyle: greyFontStyle,
+                    hintText: 'Type Your Email Address'),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.fromLTRB(defaultMargin, 10, defaultMargin, 6),
+              child: Text(
+                "Password",
+                style: blackFontStyle2,
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: Colors.black,
+                ),
+              ),
+              child: TextField(
+                controller: passwprdController,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintStyle: greyFontStyle,
+                    hintText: 'Type Your Password'),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 45,
+              margin: EdgeInsets.only(top: 24),
+              padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: mainColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Continue",
+                  style: blackFontStyle3.copyWith(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
