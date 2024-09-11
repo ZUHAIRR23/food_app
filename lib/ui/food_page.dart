b0120c17207e19b9b4c808a04c511082bca73a23
Abdurahman Zuhair <abdurrahman.zuh@gmail.com>
2024-09-11 09:48:34 +0700

    Nambah Materi

--- lib/ui/food_page.dart
+++ lib/ui/food_page.dart
@@ -14,7 +14,7 @@ class _FoodPageState extends State<FoodPage> {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-      //   Header
+        //   Header
         Container(
           color: Colors.white,
           height: 100,
@@ -22,10 +22,48 @@ class _FoodPageState extends State<FoodPage> {
           padding: EdgeInsets.symmetric(horizontal: defaultMargin),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Food Market",
+                    style: blackFontStyle1,
+                  ),
+                  Text(
+                    "Let's Go Eat!",
+                    style: blackFontStyle2,
+                  ),
+                ],
+              ),
+              Container(
+                width: 65,
+                height: 65,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        "https://i.pinimg.com/564x/35/66/fb/3566fb6a1401703c145e9662bc3a7958.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-      //   Card Food
-      //   Tab Layout
+        //   Card Food
+        Container(
+          height: 220,
+          width: double.infinity,
+          margin: EdgeInsets.symmetric(
+            vertical: defaultMargin,
+          ),
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+          ),
+        )
+        //   Tab Layout
       ],
     );
   }
