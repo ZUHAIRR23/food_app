7bbd538dd82012a940f0abf8357b5cc101c20a76
Abdurahman Zuhair <abdurrahman.zuh@gmail.com>
2024-09-12 15:00:30 +0700

    Nambah Materi

--- lib/model/transaction.dart
+++ lib/model/transaction.dart
@@ -4,9 +4,25 @@ enum TransactionStatus {
   delivered,
   on_delivery,
   pending,
-  canceled
+  canceled,
 }
 
 class Transaction {
+  final int? id;
+  final Food? food;
+  final int? quantity;
+  final int? total;
+  final DateTime? dateTime;
+  final TransactionStatus? status;
+  final User? user;
 
+  Transaction({
+    this.id,
+    this.food,
+    this.quantity,
+    this.total,
+    this.dateTime,
+    this.status,
+    this.user,
+  });
 }
--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,1 +1,23 @@
 part of 'models.dart';
+
+class User {
+  final int? id;
+  final String? name;
+  final String? email;
+  final String? address;
+  final String? houseNumber;
+  final String? phoneNumber;
+  final String? city;
+  final String? picturePath;
+
+  User({
+    this.id,
+    this.name,
+    this.email,
+    this.address,
+    this.houseNumber,
+    this.phoneNumber,
+    this.city,
+    this.picturePath,
+  });
+}
