696e195890fde652531971e1b09fd3f1b149fbd2
Abdurahman Zuhair <abdurrahman.zuh@gmail.com>
2024-09-16 14:06:07 +0700

    Nambah Materi

--- lib/ui/payment_page.dart
+++ lib/ui/payment_page.dart
@@ -26,7 +26,7 @@ class PaymentPage extends StatelessWidget {
               children: <Widget>[
                 Text(
                   'Item Orders',
-                  style: blackFontStyle3,
+                  style: blackFontStyle3.copyWith(fontSize: 16),
                 ),
                 SizedBox(
                   height: 12,
@@ -58,8 +58,12 @@ class PaymentPage extends StatelessWidget {
                             overflow: TextOverflow.ellipsis,
                           ),
                         ),
-                        RatingStars(
-                          rate: transaction.food!.rate,
+                        Text(
+                          NumberFormat.currency(
+                            symbol: 'IDR ',
+                            decimalDigits: 0,
+                            locale: 'id_ID',
+                          ).format(transaction.food!.price),
                         ),
                       ],
                     ),
@@ -71,6 +75,242 @@ class PaymentPage extends StatelessWidget {
                       ),
                     ),
                   ],
+                ),
+                SizedBox(height: 12),
+                //  Detail Transaction
+                Divider(
+                  thickness: 1,
+                  color: Colors.black,
+                ),
+                Text(
+                  'Detail Transaction',
+                  style: blackFontStyle3.copyWith(fontSize: 16),
+                ),
+                // Makanam
+                SizedBox(height: 12),
+                Row(
+                  children: [
+                    Text(
+                      transaction.food!.name!,
+                      style: blackFontStyle3,
+                    ),
+                    Spacer(),
+                    Text(
+                      NumberFormat.currency(
+                        symbol: 'IDR ',
+                        decimalDigits: 0,
+                        locale: 'id_ID',
+                      ).format(transaction.food!.price),
+                    )
+                  ],
+                ),
+                // Quantity
+                SizedBox(height: 12),
+                Row(
+                  children: [
+                    Text(
+                      'Quantity',
+                      style: blackFontStyle3,
+                    ),
+                    Spacer(),
+                    Text(
+                      '${transaction.quantity.toString()} item(s)',
+                    ),
+                  ],
+                ),
+                // Sub Total
+                SizedBox(height: 12),
+                Row(
+                  children: [
+                    Text(
+                      'Sub Total',
+                      style: blackFontStyle3,
+                    ),
+                    Spacer(),
+                    Text(
+                      NumberFormat.currency(
+                        symbol: 'IDR ',
+                        decimalDigits: 0,
+                        locale: 'id_ID',
+                      ).format(
+                          transaction.food!.price! * transaction.quantity!),
+                    ),
+                  ],
+                ),
+                Divider(
+                  thickness: 1,
+                  color: Colors.black,
+                ),
+                // Tax
+                SizedBox(height: 12),
+                Row(
+                  children: [
+                    Text(
+                      'Tax 10%',
+                      style: blackFontStyle3,
+                    ),
+                    Spacer(),
+                    Text(
+                      NumberFormat.currency(
+                        symbol: 'IDR ',
+                        decimalDigits: 0,
+                        locale: 'id_ID',
+                      ).format(transaction.food!.price! *
+                          transaction.quantity! *
+                          0.1),
+                    ),
+                  ],
+                ),
+                // Driver / Shipping Cost
+                SizedBox(height: 12),
+                Row(
+                  children: [
+                    Text(
+                      'Shipping Cost',
+                      style: blackFontStyle3,
+                    ),
+                    Spacer(),
+                    Text(
+                      NumberFormat.currency(
+                        symbol: 'IDR ',
+                        decimalDigits: 0,
+                        locale: 'id_ID',
+                      ).format(50000),
+                    ),
+                  ],
+                ),
+                Divider(
+                  thickness: 1,
+                  color: Colors.black,
+                ),
+                // Total
+                SizedBox(height: 12),
+                Row(
+                  children: [
+                    Text(
+                      'Total',
+                      style: blackFontStyle2,
+                    ),
+                    Spacer(),
+                    Text(
+                      NumberFormat.currency(
+                        symbol: 'IDR ',
+                        decimalDigits: 0,
+                        locale: 'id_ID',
+                      ).format(transaction.total),
+                    )
+                  ],
+                ),
+                SizedBox(height: defaultMargin),
+                Divider(
+                  thickness: 1,
+                  color: Colors.black,
+                ),
+                Text(
+                  'Deliver to:',
+                  style: blackFontStyle3,
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text(
+                      'Nama Penerima:',
+                      style: blackFontStyle3,
+                    ),
+                    Spacer(),
+                    Text(
+                      mockUser.name!,
+                      style: blackFontStyle3,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text(
+                      'Email Penerima:',
+                      style: blackFontStyle3,
+                    ),
+                    Spacer(),
+                    Text(
+                      mockUser.email!,
+                      style: blackFontStyle3,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text(
+                      'Phone Number:',
+                      style: blackFontStyle3,
+                    ),
+                    Spacer(),
+                    Text(
+                      mockUser.phoneNumber!,
+                      style: blackFontStyle3,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text(
+                      'Address:',
+                      style: blackFontStyle3,
+                    ),
+                    Spacer(),
+                    Text(
+                      mockUser.address!,
+                      style: blackFontStyle3,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text(
+                      'House Number:',
+                      style: blackFontStyle3,
+                    ),
+                    Spacer(),
+                    Text(
+                      mockUser.houseNumber!,
+                      style: blackFontStyle3,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text(
+                      'City:',
+                      style: blackFontStyle3,
+                    ),
+                    Spacer(),
+                    Text(
+                      mockUser.city!,
+                      style: blackFontStyle3,
+                    ),
+                  ],
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 45,
+                  margin: EdgeInsets.only(top: 12),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: mainColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'Order Now',
+                      style: blackFontStyle3.copyWith(color: Colors.white),
+                    ),
+                  ),
                 ),
               ],
             ),
