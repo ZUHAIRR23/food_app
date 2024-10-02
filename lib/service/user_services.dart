057812927896dd01cb09e917a11b2fc875b9431a
Abdurahman Zuhair <abdurrahman.zuh@gmail.com>
2024-10-02 09:49:27 +0700

    Nambah Materi

--- lib/service/user_services.dart
+++ lib/service/user_services.dart
@@ -9,29 +9,27 @@ class UserServices {
 
     String url = baseUrl + '/login';
 
-    var response = await client.post(
-      Uri.parse(url),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode(
-        <String, String>{
-          'email': email,
-          'password': password,
+    var response = await client.post(Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
         },
-      )
-    );
+        body: jsonEncode(
+          <String, String>{
+            'email': email,
+            'password': password,
+          },
+        ));
 
-    if(response.statusCode != 200) {
+    if (response.statusCode != 200) {
       return ApiReturnValue(message: 'Login failed, please try again');
     }
 
     var data = jsonDecode(response.body);
 
     User.token = data['data']['token'];
-    User value = User.fromJson(data['data']['token']);
+    User value = User.fromJson(data['data']['user']);
 
-    return ApiReturnValue(value: mockUser);
+    return ApiReturnValue(value: value);
 
     // await Future.delayed(Duration(milliseconds: 500));
 
@@ -40,5 +38,81 @@ class UserServices {
 
     // Login Gagal
     // return ApiReturnValue(message: "Email atau password salah");
+  }
+
+  static Future<ApiReturnValue<User>> signUp(User user, String password,
+      {File? pictureFile, http.Client? client}) async {
+    if (client == null) {
+      client = http.Client();
+    }
+
+    String url = baseUrl + '/register';
+
+    var response = await http.post(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: {
+        'name': user.name,
+        'email': user.email,
+        'password': password,
+        'password_confirmation': password,
+        'address': user.address,
+        'city': user.city,
+        'houseNumber': user.houseNumber,
+        'phoneNumber': user.phoneNumber,
+      },
+    );
+
+    if (response.statusCode != 200) {
+      return ApiReturnValue(message: 'Register failed, please try again');
+    }
+
+    var data = jsonDecode(response.body);
+
+    User.token = data['data']['token'];
+    User value = User.fromJson(data['data']['user']);
+
+    // Upload Picture
+    if (pictureFile != null) {
+      ApiReturnValue<String> result = await uploadPicturePath(pictureFile);
+    }
+
+    return ApiReturnValue();
+  }
+
+  static Future<ApiReturnValue<String>> uploadPicturePath(File pictureFile,
+      {http.MultipartRequest? request}) async {
+    String url = baseUrl + '/user/photo';
+
+    var uri = Uri.parse(url);
+
+    if (request == null) {
+      request = http.MultipartRequest('POST', uri)
+        ..headers['Content-Type'] = 'application/json'
+        ..headers['Authorization'] = 'Barer ${User.token}';
+    }
+
+    var multiPartFile =
+        await http.MultipartFile.fromPath('file', pictureFile.path);
+
+    request.files.add(multiPartFile);
+
+    var response = await request.send();
+
+    if (response.statusCode == 200) {
+      String responseBody = await response.stream.bytesToString();
+
+      var data = jsonDecode(responseBody);
+
+      String imagePath = data['data'][0];
+
+      return ApiReturnValue(value: imagePath);
+    } else {
+      return ApiReturnValue(message: 'Upload picture failed, please try again');
+    }
+
+    return ApiReturnValue();
   }
 }
